1674928bfc4f10394ddf4930264d1958844ca486
MarcusFSS <batofilippe@hotmail.com>
2025-12-12 15:25:58 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+from datetime import date
+
+st.set_page_config(
+    page_title="DailyFit",
+    page_icon="logo.png",
+    layout="centered"
+)
+
+st.title("📓 Diário Fitness")
+st.write("App em construção")
